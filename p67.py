e7c93c00fd7c03ae7c4230bd9280ecede7a942f8
Raj-Kumar-Chaurasiya <rajchaurasiya9236@gamil.com>
2025-09-08 22:18:03 +0530

    Add new code

---
+++ p67.py
@@ -0,0 +1,4 @@
+# Write a program to Capitalize the string.
+
+a="ram"
+print(a.capitalize())
---
+++ p70.py
@@ -0,0 +1,6 @@
+# Write a program by using find function of string.
+
+a = "Ram is going to market."
+b = "to"
+position=a.find(b,0,len(a)-1)
+print("Position find =",position)
